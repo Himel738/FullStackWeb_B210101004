6b0005fd4adc744cb49e97a16413adf0beda38e4
KutuMia <sohanurhimel@gmail.com>
2026-07-18 14:00:15 +0600

    :(

--- DjangoFrameWorkLearning/drf-project/api/urls.py
+++ DjangoFrameWorkLearning/drf-project/api/urls.py
@@ -4,5 +4,8 @@ from .import views
 
 urlpatterns = [
     path("students/", views.studentsView, name="students"),
-    path('students/<int:pk>', views.studentDetailView)
+    path('students/<int:pk>', views.studentDetailView),
+
+    path('employees/', views.Employees.as_view()),
+    path('employees/<int:pk>', views.EmployeeDetail.as_view())
 ]
